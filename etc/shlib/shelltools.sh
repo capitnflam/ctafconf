bcc04a7323438380ad0a86e8824cacfec51bf57e
capitN.flam <capitnflam@gmail.com>
2020-01-30 22:19:44 +0100

    remove env_(un)set for export & unset

--- etc/shlib/shelltools.sh
+++ etc/shlib/shelltools.sh
@@ -13,13 +13,6 @@
 ###
 
 ##PORTABLE SHELL COMMAND
-# env_set var value
-# env_unset var
-# env_append var value
-# env_prepend var value
-# env_ifnull var value
-# env_ifndef var value
-
 # alias_set var value
 # alias_unset var
 
@@ -28,31 +21,6 @@
 
 #######################
 test "x$1" = "xcsh" && goto csh_functions
-
-#echo "Shell mode: SH"
-env_set() {
-  eval "$1='$2'" export "$1";
-}
-
-env_unset() {
-  unset "$@"
-}
-
-env_append() {
-  eval $1=\${$1:+\$$1${2:+:}}\$2 export $1;
-}
-
-env_prepend() {
-  eval $1=\$2\${$1:+${2:+:}\$$1} export $1;
-}
-
-env_ifnull() {
-  eval $1=\${$1:-\$2} export $1;
-}
-
-env_ifndef() {
-  eval $1=\${$1-\$2} export $1;
-}
 
 alias_set() {
   alias -- "$1=$2"
@@ -84,16 +52,6 @@ return
 
 csh_functions:
 #echo "Shell mode: CSH"
-
-alias env_set 'setenv \!:1 \!:2\'
-alias env_unset 'unsetenv \!:1'
-alias env_ifndef  'if (! $?\!:1) setenv \!:1 \!:2'
-alias env_ifnull  'if (! $?\!:1) setenv \!:1; eval " \\
-      test X != '\''X$\!:1'\'' || setenv \!:1 \!:2"'
-alias env_append  'if (! $?\!:1) setenv \!:1; eval " \\
-      test X != '\''X$\!:1'\'' && setenv \!:1 $\!:1\:\!:2 || setenv \!:1 \!:2"'
-alias env_prepend 'if (! $?\!:1) setenv \!:1; eval " \\
-      test X != '\''X$\!:1'\'' && setenv \!:1 \!:2\:$\!:1 || setenv \!:1 \!:2"'
 
 alias alias_set 'alias'
 alias alias_unset 'unalias'
